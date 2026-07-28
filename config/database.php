403c7db5566efdee27f6879bb96e87313bd7d6ed
mfahrizal423 <mfahrizalrizal423@gmail.com>
2026-07-28 22:21:37 +0700

    Add nixpacks.toml and clean database config for automated Railway deployment

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL', env('MYSQL_URL')),
+            'url' => env('DB_URL'),
             'host' => env('DB_HOST', env('MYSQLHOST', '127.0.0.1')),
             'port' => env('DB_PORT', env('MYSQLPORT', '3306')),
             'database' => env('DB_DATABASE', env('MYSQLDATABASE', 'laravel')),
